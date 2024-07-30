74873b67ec4a43c85aeee7c293d8b6f77f668246
vaishnaviii9 <102385162+vaishnaviii9@users.noreply.github.com>
2024-07-30 09:20:23 +0000

    variables and datatypes

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId =291732
+let accountEmail = "vaish@gmail.com"
+var accountPassword ="23980392"
+accountCity ="Pune"
+let accountState;
+// prefer not to use var( issue in block and function scope)
+//accountId = 2
+accountEmail="vt@google.com"
+accountPassword ="2203981"
+accountCity="bengaluru"
+console.log(accountId); 
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
