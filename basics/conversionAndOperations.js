4c2cd1406b9290bcb7f0440178a20c292128c365
vaishnaviii9 <102385162+vaishnaviii9@users.noreply.github.com>
2024-07-30 09:42:25 +0000

    conversions

---
+++ basics/conversionAndOperations.js
@@ -0,0 +1,36 @@
+// let score =33
+// let a="33"
+// let b="33abc"
+
+// console.log("type of score: "+ typeof score);
+// console.log("type of a: "+typeof a);
+
+// let valueInNumber = Number(a)
+// let val= Number(b)
+
+// console.log("type of valueInNumber: "+typeof valueInNumber);
+// console.log("type of val: "+typeof val);
+// ''
+// console.log("Val: "+val);
+
+// let c=null
+// let c1= Number(c)
+// console.log("type of c1: "+typeof c1);
+// console.log("C1: "+c1);
+// "33" -33
+// "33abc" - NaN
+// true-1, false-0
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn );
+
+// 1- true, 0-false
+// "" -false
+// "abcd" - true
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
