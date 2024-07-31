c59b8e3e1190b85bbb31c8e5e9f7ef50caf3cead
vaishnaviii9 <102385162+vaishnaviii9@users.noreply.github.com>
2024-07-31 07:42:00 +0000

    conversion

--- basics/conversionAndOperations.js
+++ basics/conversionAndOperations.js
@@ -30,7 +30,26 @@
 // "" -false
 // "abcd" - true
 
-let someNumber =33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber =33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value=3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let str1="hello"
+let str2=" bye"
+
+let str3= str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
