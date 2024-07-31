046b3b5b63b9cc38af8aaa6fd7e1ababd4d4e9c5
vaishnaviii9 <102385162+vaishnaviii9@users.noreply.github.com>
2024-07-31 07:51:50 +0000

    conversion

--- basics/conversionAndOperations.js
+++ basics/conversionAndOperations.js
@@ -35,21 +35,41 @@
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
 
-let value=3
-let negValue = -value
-console.log(negValue);
+// let value=3
+// let negValue = -value
+// console.log(negValue);
 
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(2/2);
-console.log(2**2);
-console.log(2%2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
 
-let str1="hello"
-let str2=" bye"
+// let str1="hello"
+// let str2=" bye"
 
-let str3= str1 + str2
-console.log(str3);
+// let str3= str1 + str2
+// console.log(str3);
 
-console.log("1" + 2);
+// console.log("1" + 2);
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// let num1,num2,num3
+// num1= num2= num3 =2+2
+
+// let gameCounter =100
+// gameCounter++;
+// console.log(gameCounter);
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
