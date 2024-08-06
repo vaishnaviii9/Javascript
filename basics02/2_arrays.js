d0e9ed6f6889bb305095d97ab79aa380540b82b7
vaishnaviii9 <102385162+vaishnaviii9@users.noreply.github.com>
2024-08-06 15:29:33 +0000

    arrays 2

---
+++ basics02/2_arrays.js
@@ -0,0 +1,14 @@
+const marvel =["thor","spiderman","ironman"]
+const dc =["superman","batman","flash"]
+
+marvel.push(dc)
+
+// console.log(marvel);
+// console.log(dc);
+
+// console.log(marvel[3]);
+// console.log(marvel[3][1]);
+
+marvel.concat(dc)
+console.log(marvel);
+console.log(dc);
