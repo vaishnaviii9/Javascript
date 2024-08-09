d47dddde4c401c5a05a977941fccc4534c9eeb18
vaishnaviii9 <102385162+vaishnaviii9@users.noreply.github.com>
2024-08-09 13:43:02 +0000

    functions with objects

--- basics02/functions.js
+++ basics02/functions.js
@@ -1,7 +1,7 @@
 // functions
 
-function sayMyName(){
-console.log("Reference");
+function sayMyName() {
+    console.log("Reference");
 
 }
 
@@ -14,27 +14,27 @@ console.log("Reference");
 // function addNumbers(num1, num2){ //num1,num2- parameters
 //     let result = num1+num2
 //     // console.log(result);
-    
+
 //     return result
 // }
 
 
-function addNumbers(num1, num2){ //num1,num2- parameters
-    return num1+num2
+function addNumbers(num1, num2) { //num1,num2- parameters
+    return num1 + num2
 }
 
 // addNumbers(4,9) //4,9-arguments
 // addNumbers(4,"9")
 // addNumbers(4,null)
 
-const result = addNumbers(4,9)
-console.log("Result: ",result);
+const result = addNumbers(4, 9)
+console.log("Result: ", result);
 
-function loginUserMessage(username = "user"){
-    if(!username){
+function loginUserMessage(username = "user") {
+    if (!username) {
         console.log("Please enter a username");
-        return 
-            }
+        return
+    }
     return `${username} just logged in`
 }
 
@@ -42,3 +42,48 @@ function loginUserMessage(username = "user"){
 // console.log(loginUserMessage(""))
 console.log(loginUserMessage())
 
+
+// rest operator / spread operator
+function calculateCartPrice(...args) {
+    let sum = 0;
+    for (let i of args) {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+const totalPrice = calculateCartPrice(200, 300, 400);
+console.log(`${totalPrice} is the total price `);
+
+function example(val1, val2, ...num1) {
+    return num1
+}
+console.log(example(200, 300, 500, 30000));
+
+const user = {
+    username: "user",
+    price: 300
+}
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+
+}
+
+handleObject(user)
+
+handleObject(
+    {
+        username: "abcd",
+        price: 499
+    }
+)
+
+// arrays
+
+const myNewArray =[100,200,499,392]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([283094,129890,2908,302,2040]));
