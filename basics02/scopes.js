18170d03016bb5d240b63ae79150d8a966fe4bc9
vaishnaviii9 <102385162+vaishnaviii9@users.noreply.github.com>
2024-08-09 13:53:05 +0000

    let, var and const

---
+++ basics02/scopes.js
@@ -0,0 +1,26 @@
+// let,var,const
+
+// var c=300
+let a=300
+
+if (true) {
+
+    let a = 10 
+    const b = 20
+    var c = 30 //override 300 
+    console.log("Inner a:",a); 
+
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+ console.log(a); 
+// console.log(b); 
+// console.log(c);
+
+
+// global scope in console(inspect) and in vscode env is different
+
